3c1c15533c75af52772b5befe9bd5868ae66de51
Dennis Muensterer <dennismuensterer@gmail.com>
2019-06-07 08:55:38 +0200

    fixed brewfile

--- brew.sh
+++ brew.sh
@@ -3,160 +3,132 @@
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
-# brew update
-#
-# # Upgrade any already-installed formulae.
-# brew upgrade
-#
-# # Save Homebrew’s installed location.
-# BREW_PREFIX=$(brew --prefix)
-#
-# # Install GNU core utilities (those that come with macOS are outdated).
-# # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-# brew install coreutils
-# ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
-#
-# # Install some other useful utilities like `sponge`.
-# brew install moreutils
-# # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-# brew install findutils
-# # Install GNU `sed`, overwriting the built-in `sed`.
-# brew install gnu-sed
-# # Install Bash 4.
-# brew install bash
-# brew install bash-completion2
-#
-# # Switch to using brew-installed bash as default shell
-# if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-#   echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-#   chsh -s "${BREW_PREFIX}/bin/bash";
-# fi;
-#
-# # Install downloaders and installers
-# brew install wget
-# brew install cask
-# brew install mas
-#
-# # Install languages and frameworks
-# brew install lua
-# brew install node
-# brew install php
-# brew install python
-# brew install ruby
-# brew install rbenv
-#
-# # Install version control
-# brew install git
-# brew install hub
-# brew install git-lfs
-#
-# # Install GnuPG to enable PGP-signing commits.
-# brew install gnupg
-#
-# # Install editors
-# brew install vim
-# brew install zsh
-# brew install fish
-#
-# # Install web tools.
-# brew install openssh
-#
-# # Install fun stuff
-#
-# # Install font tools.
-# brew tap bramstein/webfonttools
-# brew install sfnt2woff
-# brew install sfnt2woff-zopfli
-# brew install woff2
-#
-# # Install network utils
-# brew install aircrack-ng
-# brew install bfg
-# brew install binutils
-# brew install binwalk
-# brew install cifer
-# brew install dex2jar
-# brew install dns2tcp
-# brew install fcrackzip
-# brew install foremost
-# brew install geoip
-# brew install hashpump
-# brew install hydra
-# brew install john
-# brew install knock
-# brew install netpbm
-# brew install nmap
-# brew install pngcheck
-# brew install socat
-# brew install speedtest_cli
-# brew install sqlmap
-# brew install tcpflow
-# brew install tcpreplay
-# brew install tcptrace
-# brew install ucspi-tcp # `tcpserver` etc.
-# brew install wifi-password
-# brew install xpdf
-# brew install xz
-# brew install youtube-dl
-#
-# # Install other useful binaries.
-# brew install ack
-# brew install calc
-# brew install cowsay
-# brew install dockutil
-# brew install fish
-# brew install gibo
-# brew install gmp
-# brew install grep
-# brew install htop
-# brew install imagemagick
-# brew install lynx
-# brew install mackup
-# brew install multimarkdown
-# brew install p7zip
-# brew install pigz
-# brew install pv
-# brew install rename
-# brew install rlwrap
-# brew install screen
-# brew install ssh-copy-id
-# brew install tree
-# brew install thefuck
-# brew install vbindiff
-# brew install watch
-# brew install watchman
-# brew install yarn
-# brew install zopfli
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed
+# Install Bash 4.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Install downloaders and installers
+brew install wget
+brew install cask
+brew install mas
+
+# Install languages and frameworks
+brew install lua
+brew install node
+brew install php
+brew install python
+brew install ruby
+brew install rbenv
+
+# Install version control
+brew install git
+brew install hub
+brew install git-lfs
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install editors
+brew install vim
+brew install zsh
+brew install fish
+
+# Install web tools.
+brew install openssh
+
+# Install fun stuff
+
+# Install font tools.
+brew tap bramstein/webfonttools
+brew install sfnt2woff
+brew install sfnt2woff-zopfli
+brew install woff2
+
+# Install network utils
+brew install aircrack-ng
+brew install bfg
+brew install binutils
+brew install binwalk
+brew install cifer
+brew install dex2jar
+brew install dns2tcp
+brew install fcrackzip
+brew install foremost
+brew install geoip
+brew install hashpump
+brew install hydra
+brew install john
+brew install knock
+brew install netpbm
+brew install nmap
+brew install pngcheck
+brew install socat
+brew install speedtest_cli
+brew install sqlmap
+brew install tcpflow
+brew install tcpreplay
+brew install tcptrace
+brew install ucspi-tcp # `tcpserver` etc.
+brew install wifi-password
+brew install xpdf
+brew install xz
+brew install youtube-dl
+
+# Install other useful binaries.
+brew install ack
+brew install calc
+brew install cowsay
+brew install dockutil
+brew install fish
+brew install gibo
+brew install gmp
+brew install grep
+brew install htop
+brew install imagemagick
+brew install lynx
+brew install mackup
+brew install multimarkdown
+brew install p7zip
+brew install pigz
+brew install pv
+brew install rename
+brew install rlwrap
+brew install screen
+brew install ssh-copy-id
+brew install tree
+brew install thefuck
+brew install vbindiff
+brew install watch
+brew install watchman
+brew install yarn
+brew install zopfli
 
 # Cask install GUI apps
-# brew cask install aerial
-# brew cask install arduino
-# brew cask install atom
-# brew cask install amazon-music
-# brew cask install android-studio
-# brew cask install bettertouchtool
-# brew cask install boom-3d
-# brew cask install docker
-# brew cask install dropbox
-# brew cask install duet
-# brew cask install github
-# brew cask install google-chrome
-# brew cask install google-backup-and-sync
-# brew cask install iterm2
-# brew cask install java
-# brew cask install keyboard-cleaner
-# brew cask install midi-monitor
-# brew cask install openemu
-# brew cask install postman
-# brew cask install sketch
-# # brew cask install slack
-# brew cask install sourcetree
-# brew cask install spotify
-# brew cask install steam
-# brew cask install sublime-text
-# brew cask install the-unarchiver
-# brew cask install qlmarkdown
-
 declare -a cask_apps=(
   aerial
   arduino
