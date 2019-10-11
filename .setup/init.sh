bd14f1271f1afb1e07a4e371db4ecc492179d066
Dennis Muensterer <dm@hpm.agency>
2019-10-11 10:28:01 +0200

    Update init script with Mac-CLI and nvm

--- .setup/init.sh
+++ .setup/init.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/guarinogabriel/mac-cli/master/mac-cli/tools/install)"
