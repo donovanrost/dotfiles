2b4342e46cc00e8deb34481d8d166a55683cf7ef
Dennis Muensterer <dm@hpm.agency>
2020-01-06 11:33:54 +0100

    Make init script more flexible

--- setup/init.sh
+++ setup/init.sh
@@ -1,18 +1,75 @@
 #!/usr/bin/env bash
 
-# Run installers
-bundle install
-pip install -r requirements.txt
-npm install -g $(cat npmfile)
+if [ "$1" == "-h" ] ; then
+	echo "Usage: init"
+	echo "Options: "
+	echo "-h  Show this message"
+	echo "-a  Install all the options (gem, pip, npm, nvm, oh-my-zsh, oh-my-fish)"
+	echo "-g  Install Gemfile"
+	echo "-n  Install node apps & nvm"
+	echo "-p  Install python requirements"
+	return
+elif [ "$#" -gt 1 ]; then
+	echo "Too many parameters"
+	return
+fi
 
-# nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+function gem() {
+  echo "Installing gems"
+  bundle install
+}
 
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+function pip() {
+  echo "Installing pip requirements"
+  pip install -r requirements.txt
+}
 
-# fish
-curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
+function npm() {
+  echo "Installing npm apps"
+  npm install -g $(cat npmfile)
+}
 
-# mac-cli
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/guarinogabriel/mac-cli/master/mac-cli/tools/install)"
+function nvm() {
+  echo "Installing nvm"
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+}
+
+function omz() {
+  echo "Installing oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+
+function omf() {
+  echo "Installing oh-my-fish"
+  curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
+}
+
+function mac-cli() {
+  echo "Installing mac-cli"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/guarinogabriel/mac-cli/master/mac-cli/tools/install)"
+}
+
+if [ "$1" == "-a" ]; then
+	gem
+  pip
+  npm
+  omz
+  omf
+  mac-cli
+elif [[ "$1" == "-g" ]]; then
+	gem
+elif [[ "$1" == "-n" ]]; then
+	npm
+  nvm
+elif [[ "$1" == "-p" ]]; then
+	pip
+elif [[ "$1" == "-o" ]]; then
+	omz
+  omf
+  mac-cli
+else
+  gem
+  pip
+  npm
+	echo "For specific categories, run again with appropriate flags (-h for more info)"
+fi;
