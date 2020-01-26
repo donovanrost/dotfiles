55c155449c6d437ce4e69ea47444ddb7be1c7dce
Dennis Muensterer <dennismuensterer@gmail.com>
2020-01-26 23:27:27 +0100

    Move and link fish

--- .config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-# ~/.config/fish/config.fish
-
-# source ~/.config/fish/path.fish
-# source ~/.config/fish/aliases.fish
-# source ~/.config/fish/functions.fish
-
-starship init fish | source
---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+# ~/.config/fish/config.fish
+
+# source ~/.config/fish/path.fish
+# source ~/.config/fish/aliases.fish
+# source ~/.config/fish/functions.fish
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+starship init fish | source
