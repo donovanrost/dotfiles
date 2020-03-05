3a64f4517da552c364b56b65b6af8ad8ef480545
Dennis Muensterer <dm@hpm.agency>
2020-03-05 16:41:08 +0100

    Add shellswitch function

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -2,7 +2,6 @@
 
 source ~/.dotfiles/shell/profile
 source ~/.config/fish/aliases.fish
-source ~/.config/fish/functions.fish
 
 starship init fish | source
 status --is-interactive; and source (rbenv init -|psub)
