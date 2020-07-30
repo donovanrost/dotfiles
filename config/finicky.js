050a2fc9606684b4e89683f55af151d098d2ac93
Dennis Muensterer <dennismuensterer@gmail.com>
2020-07-31 00:16:47 +0200

    👌Simplify finicky

--- config/finicky.js
+++ config/finicky.js
@@ -1,3 +1,5 @@
+const workUrls = ['hpm.agency', 'hpm.digital', /.*\.hpm.agency$/]
+const videoCalling = ['zoom', 'zoom.us', /.*\.zoom.us$/, 'meet.jit.si']
 module.exports = {
   defaultBrowser: 'Safari',
   options: {
@@ -6,45 +8,22 @@ module.exports = {
   },
   handlers: [
     {
-      // Open links in Safari when the option key is pressed
-      // Valid keys are: shift, option, command, control, capsLock, and function.
-      // Please note that control usually opens a tooltip menu instead of visiting a link
       match: ({ keys }) => keys.option,
       browser: 'Google Chrome'
     },
-    // {
-    //   // Open any link clicked in Slack in Safari
-    //   match: ({ sourceBundleIdentifier }) =>
-    //     sourceBundleIdentifier === 'com.tinyspeck.slackmacgap',
-    //   browser: 'Google Chrome'
-    // },
     {
-      // Open work urls in Chrome
-      match: finicky.matchHostnames(['hpm.agency', 'hpm.digital', /.*\.hpm.agency$/]),
+      // Open in Chrome
+      match: finicky.matchHostnames([...workUrls, ...videoCalling, 'localhost', 'chrome.google.com']),
       browser: 'Google Chrome'
     },
     {
-      // Open work urls in Chrome
-      match: finicky.matchHostnames(['zoom', 'zoom.us', /.*\.zoom.us$/, 'meet.jit.si']),
-      browser: 'Google Chrome'
-    },
-    {
-      // Open work urls in Chrome
+      // Open youtube in safari
       match: finicky.matchHostnames(['youtube.com', 'youtube.de', /.*\.youtube.com/]),
       browser: 'Safari'
     },
-    // {
-    //   // Avoid being rickrolled
-    //   match: [
-    //     'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-    //     'https://www.youtube.com/watch?v=oHg5SJYRHA0'
-    //   ],
-    //   url: 'about:blank'
-    // },
     {
-      match: ['http://example.com'],
-      // Don't open any browser for this url, effectively blocking it
-      browser: null
+      match: ({ sourceBundleIdentifier }) => sourceBundleIdentifier === 'com.tinyspeck.slackmacgap',
+      browser: 'Google Chrome'
     }
   ]
 }
